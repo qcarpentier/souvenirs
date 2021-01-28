a07684652b831bb63ac10ed659706535a970cdb8
Quentin Carpentier <quentin.carpentier@outlook.be>
2021-01-28 15:49:25 +0100

    ✨ Create a Souvenir

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -16,11 +16,16 @@ export const createPost = async (req, res) => {
   const post = req.body;
   const newPost = new PostMessage(post);
 
+  console.log("Reaching server...");
+
   try {
     await newPost.save();
+    console.log("Trying to save into Db");
 
-    req.status(201).json(newPost);
+    res.status(201).json(newPost);
   } catch (error) {
+    console.error("Something went wrong");
+
     res.status(404).json({ message: error.message });
   }
 };
