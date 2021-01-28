2fbd864ee0cb763840c2b2772c417bc3a74f2f06
Quentin Carpentier <quentin.carpentier@outlook.be>
2021-01-28 12:51:07 +0100

    💄 material-ui style setup for each component

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,18 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import souvenirs from "./images/souvenirs.png";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Souvenirs
         </Typography>
-        <img src={souvenirs} alt="souvenirs" heigth="60" />
+        <img className={classes.image} src={souvenirs} alt="souvenirs" height="60" />
       </AppBar>
       <Grow in>
         <Container>
