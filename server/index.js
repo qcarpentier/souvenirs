0e904f9ed84827effe6c86a0d535a391e50c5133
Quentin Carpentier <quentin.carpentier@outlook.be>
2021-01-28 10:57:46 +0100

    🚧 Adding routes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
