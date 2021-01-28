2bc92fbef98d7abec8e8cea03f584c706e2f0776
Quentin Carpentier <quentin.carpentier@outlook.be>
2021-01-28 11:30:54 +0100

    🚧 Components and materials-ui structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import souvenirs from "./images/souvenirs.png";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Souvenirs
+        </Typography>
+        <img src={souvenirs} alt="souvenirs" heigth="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
